b151d08ce977633cbfa5192ac66d4ce2109493c4
WhisYou <70556272+yxkbf001@users.noreply.github.com>
2024-11-24 16:12:21 +0800

    Update Whis.sh

--- Whis.sh
+++ Whis.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-# 检查realm是否已安装
-if [ -f "/root/realm/realm" ]; then
-    echo "检测到realm已安装。"
-    realm_status="已安装"
-    realm_status_color="\033[0;32m" # 绿色
-else
-    echo "realm未安装。"
-    realm_status="未安装"
-    realm_status_color="\033[0;31m" # 红色
-fi
+# 定义颜色变量
+red="\033[0;31m"
+green="\033[0;32m"
+plain="\033[0m"
+
+# 更新realm状态
+update_realm_status() {
+    if [ -f "/root/realm/realm" ]; then
+        realm_status="已安装"
+        realm_status_color=$green
+    else
+        realm_status="未安装"
+        realm_status_color=$red
+    fi
+}
 
 # 检查realm服务状态
 check_realm_service_status() {
     if systemctl is-active --quiet realm; then
-        echo -e "\033[0;32m启用\033[0m" # 绿色
+        echo -e "${green}启用${plain}"
     else
-        echo -e "\033[0;31m未启用\033[0m" # 红色
+        echo -e "${red}未启用${plain}"
     fi
 }
 
@@ -35,7 +40,7 @@ show_menu() {
     echo "8. 重启服务"
     echo "0. 退出脚本"
     echo "================="
-    echo -e "realm 状态：${realm_status_color}${realm_status}\033[0m"
+    echo -e "realm 状态：${realm_status_color}${realm_status}${plain}"
     echo -n "realm 转发状态："
     check_realm_service_status
 }
@@ -102,23 +107,21 @@ uninstall_realm() {
     systemctl daemon-reload
     rm -rf /root/realm
     echo "realm已被卸载。"
-    # 更新realm状态变量
-    realm_status="未安装"
-    realm_status_color="\033[0;31m" # 红色
+    update_realm_status
 }
 
 # 删除转发规则的函数
 delete_forward() {
     echo "当前转发规则："
-    local IFS=$'\n' # 设置IFS仅以换行符作为分隔符
-    local lines=($(grep -n 'remote =' /root/realm/config.toml)) # 搜索所有包含转发规则的行
+    local IFS=$'\n'
+    local lines=($(grep -n 'remote =' /root/realm/config.toml))
     if [ ${#lines[@]} -eq 0 ]; then
         echo "没有发现任何转发规则。"
         return
     fi
     local index=1
     for line in "${lines[@]}"; do
-        echo "${index}. $(echo $line | cut -d '"' -f 2)" # 提取并显示端口信息
+        echo "${index}. $(echo $line | cut -d '"' -f 2)"
         let index+=1
     done
 
@@ -139,14 +142,12 @@ delete_forward() {
         return
     fi
 
-    local chosen_line=${lines[$((choice-1))]} # 根据用户选择获取相应行
-    local line_number=$(echo $chosen_line | cut -d ':' -f 1) # 获取行号
+    local chosen_line=${lines[$((choice-1))]}
+    local line_number=$(echo $chosen_line | cut -d ':' -f 1)
 
-    # 计算要删除的范围，从listen开始到remote结束
     local start_line=$line_number
     local end_line=$(($line_number + 2))
 
-    # 使用sed删除选中的转发规则
     sed -i "${start_line},${end_line}d" /root/realm/config.toml
 
     echo "转发规则已删除。"
@@ -163,6 +164,8 @@ add_forward() {
 listen = \"0.0.0.0:$port2\"
 remote = \"$ip:$port\"" >> /root/realm/config.toml
         
+
+
         read -p "是否继续添加(Y/N)? " answer
         if [[ $answer != "Y" && $answer != "y" ]]; then
             break
@@ -172,18 +175,58 @@ remote = \"$ip:$port\"" >> /root/realm/config.toml
 
 # 启动服务
 start_service() {
-    sudo systemctl unmask realm.service
-    sudo systemctl daemon-reload
-    sudo systemctl restart realm.service
-    sudo systemctl enable realm.service
+    systemctl unmask realm.service
+    systemctl daemon-reload
+    systemctl restart realm.service
+    systemctl enable realm.service
     echo "realm服务已启动并设置为开机自启。"
+    update_realm_status
 }
 
 # 停止服务
 stop_service() {
     systemctl stop realm
     echo "realm服务已停止。"
+    update_realm_status
 }
+
+# 重启服务
+restart_service() {
+    systemctl restart realm
+    echo "realm服务已重启。"
+    update_realm_status
+}
+
+# 更新realm
+update_realm() {
+    echo "> 检测并更新 realm"
+
+    tag_version=$(curl -Ls "https://api.github.com/repos/zhboner/realm/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+    
+    if [[ -z "$tag_version" ]]; then
+        echo -e "${red}获取 realm 版本失败，可能是由于 GitHub API 限制，请稍后再试${plain}"
+        exit 1
+    fi
+
+    echo -e "获取到 realm 最新版本: ${tag_version}，开始安装..."
+
+    wget -N --no-check-certificate -O /root/realm/realm.tar.gz "https://github.com/zhboner/realm/releases/download/${tag_version}/realm-$(arch)-unknown-linux-gnu.tar.gz"
+    
+    if [[ $? -ne 0 ]]; then
+        echo -e "${red}下载 realm 失败，请确保您的服务器可以访问 GitHub${plain}"
+        exit 1
+    fi
+
+    cd /root/realm
+    tar -xvf realm.tar.gz
+    chmod +x realm
+
+    echo -e "realm 更新成功。"
+    update_realm_status
+}
+
+# 初始化realm状态
+update_realm_status
 
 # 主循环
 while true; do
@@ -207,6 +250,16 @@ while true; do
             ;;
         6)
             uninstall_realm
+            ;;
+        7)
+            update_realm
+            ;;
+        8)
+            restart_service
+            ;;
+        0)
+            echo "退出脚本。"
+            exit 0
             ;;
         *)
             echo "无效选项: $choice"
